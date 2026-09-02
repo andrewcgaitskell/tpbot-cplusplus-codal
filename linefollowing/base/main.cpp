6e60ad999fcf5269f9db5caccea1ccd96b6c5804
andrewcgaitskell <andrew@gaitskell.com>
2026-09-02 13:00:16 +0100

    Update main.cpp

--- linefollowing/base/main.cpp
+++ linefollowing/base/main.cpp
@@ -6,6 +6,7 @@ MicroBit uBit;
 
 static const int POLL_INTERVAL_MS = 50;
 static const uint8_t TARGET_ROBOT_ID = 1;
+static const int LOOP_TIME_POLL_EVERY_N = 5; // ask for loop timing once every N sensor polls
 
 const char *lineStateToString(LineState state)
 {
@@ -33,10 +34,15 @@ int main()
     }
     uBit.serial.printf("# cleared %d stale packet(s)\r\n", cleared);
 
+    int cycle = 0;
+
     while (1)
     {
+        bool pollLoopTime = (cycle % LOOP_TIME_POLL_EVERY_N) == 0;
+        uint8_t pollCommand = pollLoopTime ? CMD_POLL_LOOP_TIME : CMD_POLL_SENSORS;
+
         uint8_t tx[ROBOT_COMMAND_SIZE];
-        int len = packRobotCommand(tx, TARGET_ROBOT_ID, CMD_POLL_SENSORS, 0, 0);
+        int len = packRobotCommand(tx, TARGET_ROBOT_ID, pollCommand, 0, 0);
         uBit.radio.datagram.send(tx, len);
 
         // Wait for the reply, bounded to ~100ms.
@@ -55,19 +61,22 @@ int main()
         if (received > 0)
         {
             RobotCommand reply;
-            if (unpackRobotCommand(rx, received, reply) && reply.command == CMD_SENSOR_REPORT)
+            if (!unpackRobotCommand(rx, received, reply))
             {
-                LineState left  = reply.value1 ? LineState::Black : LineState::White;
+                uBit.serial.printf("# unexpected reply, len=%d\r\n", received);
+            }
+            else if (reply.command == CMD_SENSOR_REPORT)
+            {
+                LineState left = reply.value1 ? LineState::Black : LineState::White;
                 LineState right = reply.value2 ? LineState::Black : LineState::White;
-
-                bool leftBlack  = left  == LineState::Black;
+                bool leftBlack = left == LineState::Black;
                 bool rightBlack = right == LineState::Black;
 
                 TrackingState state;
-                if (leftBlack && rightBlack)        state = TrackingState::L_R_line;
-                else if (!leftBlack && rightBlack)  state = TrackingState::L_unline_R_line;
-                else if (leftBlack && !rightBlack)  state = TrackingState::L_line_R_unline;
-                else                                 state = TrackingState::L_R_unline;
+                if (leftBlack && rightBlack) state = TrackingState::L_R_line;
+                else if (!leftBlack && rightBlack) state = TrackingState::L_unline_R_line;
+                else if (leftBlack && !rightBlack) state = TrackingState::L_line_R_unline;
+                else state = TrackingState::L_R_unline;
 
                 uBit.serial.printf("robot=%d left=%s right=%s state=%d\r\n",
                                     reply.robotId,
@@ -75,9 +84,14 @@ int main()
                                     lineStateToString(right),
                                     (int)state);
             }
+            else if (reply.command == CMD_LOOP_TIME_REPORT)
+            {
+                uBit.serial.printf("robot=%d loop_ms=%d max_loop_ms=%d\r\n",
+                                    reply.robotId, reply.value1, reply.value2);
+            }
             else
             {
-                uBit.serial.printf("# unexpected reply, len=%d\r\n", received);
+                uBit.serial.printf("# unexpected reply command=%d\r\n", reply.command);
             }
         }
         else
@@ -85,6 +99,8 @@ int main()
             uBit.serial.printf("# no reply after %dms\r\n", waited);
         }
 
+        cycle++;
         uBit.sleep(POLL_INTERVAL_MS);
     }
 }
+
